ab210cf2a87f128af0016587be32f1aaee5fa372
Angry Wasp <angrywasp@live.com>
2018-11-03 11:28:21 +1100

    More script fixes for multiple repo targets

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -117,7 +117,7 @@
 #define P2P_DEFAULT_WHITELIST_CONNECTIONS_PERCENT       70
 #define P2P_DEFAULT_ANCHOR_CONNECTIONS_COUNT            2
 
-#define SUPPORTED_MIN_VERSION                           version_string_to_integer("1.0.0.0")
+#define SUPPORTED_MIN_VERSION                           version_string_to_integer("0.1.0.0")
 
 #define P2P_FAILED_ADDR_FORGET_SECONDS                  (60*60)     //1 hour
 #define P2P_IP_BLOCKTIME_MAINNET                        (60*60*24)  //24 hour
